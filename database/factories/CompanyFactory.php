b5798c1739bf7f98f26a5b4ee13ae395054f21e7
Emerson Patryck <emeps@outlook.com>
2023-09-06 14:06:46 -0300

    feat: add Address, Company and User factories

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'cnpj' => fake()->numerify('##########0001'),
+            'address_id' => 1,
+            'user_id' => 1
+        ];
+    }
+}
