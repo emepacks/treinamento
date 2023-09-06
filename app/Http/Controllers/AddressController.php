894d663fd2eb4c625eec75511ecf2fd0092c17f1
Emerson Patryck <emeps@outlook.com>
2023-09-06 14:14:28 -0300

    feat: add store, update  and destroy features

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -4,46 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Address;
 use Illuminate\Http\Request;
-
 class AddressController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function __construct(
+        private readonly Address $address
+    )
     {
-        //
     }
-
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store($data)
     {
-        //
+        return $this->address->newQuery()->create($data);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Address $address)
-    {
-        //
+    public function update($data, int $id){
+        return $this->address->newQuery()->findOrFail($id)->update($data);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Address $address)
-    {
-        //
+    public function destroy(int $id){
+        return $this->address->newQuery()->findOrFail($id)->onlyTrashed();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Address $address)
-    {
-        //
-    }
 }
