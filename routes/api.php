da5700eab1809f4cfd9a4cb55ca6104e9568ea18
Emerson Patryck <emeps@outlook.com>
2023-09-06 14:05:15 -0300

    feat: add client routes and login route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login',[UserController::class, 'login']);
+Route::post('/signup',[UserController::class, 'signup']);
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::get('/client/companies/{id}', [UserController::class, 'showCompanies']);
+    Route::put('/client/{id}', [UserController::class, 'update']);
+    Route::delete('/client/{id}', [UserController::class, 'destroy']);
 });
+
+
