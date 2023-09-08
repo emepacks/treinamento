74710043616cb0c51f72f499e384574a46ad3f92
Emerson Patryck <emeps@outlook.com>
2023-09-07 22:31:59 -0300

    feat: add all routes of app

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -16,18 +17,35 @@ use App\Http\Controllers\UserController;
 */
 
 Route::post('/login', [UserController::class, 'login']);
-Route::post('/signup', [UserController::class, 'signup']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/client/companies/{id}', [UserController::class, 'showCompanies']);
+    // Dados do cliente
+    Route::get('/client', [UserController::class, 'index']);
+    // Atualizar dados do cliente
     Route::put('/client/{id}', [UserController::class, 'update']);
+    // Deletar cliente
     Route::delete('/client/{id}', [UserController::class, 'destroy']);
+    // Listar empresas que o cliente faz parte
+    Route::get('/client/companies/{id}', [UserController::class, 'showCompanies']);
 
     Route::middleware(['admin.access'])->group(
         function () {
-            Route::get('/admin/companies', function () {
-                return response()->json(['message' => auth()->user()->type]);
-            });
+            // Cadastrar cliente
+            Route::post('/signup', [UserController::class, 'signup']);
+            // Listar todas as empresas
+            Route::get('/companies', [CompanyController::class, 'index']);
+            // Listar uma empresa
+            Route::get('/companies/{id}', [CompanyController::class, 'show']);
+            // Cadastrar empresa
+            Route::post('/companies', [CompanyController::class, 'store']);
+            // Atualizar empresa
+            Route::put('/companies/{id}', [CompanyController::class, 'update']);
+            // Deletar empresa
+            Route::delete('/companies/{id}', [CompanyController::class, 'destroy']);
+            // Listar todos os clientes
+            Route::get('/company/clients/{id}', [UserController::class, 'showClients']);
+            // Adicionar cliente a empresa
+            Route::post('/company/clients/{id}', [UserController::class, 'addClient']);
         }
 
     );
