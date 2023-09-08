97a6dcfd2fe9d0be60fa72b30f3ff366f75cf250
Emerson Patryck <emeps@outlook.com>
2023-09-08 01:57:13 -0300

    chore: add token e notifiable in company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class Company extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
     public $table = 'companies';
 
     protected $fillable = [
