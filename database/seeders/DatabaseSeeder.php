8ba5add9418c446a5814ffe156b32444ec9d6ca2
Emerson Patryck <emeps@outlook.com>
2023-09-08 13:31:25 -0300

    feat: add factory of pivot table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 use App\Models\Address;
 use App\Models\Company;
 use App\Models\User;
+use App\Models\UserCompanies;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,9 +17,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         Address::factory(10)->create();
-        // Company::factory(1)->create();
+        Company::factory(10)->create();
         User::factory(10)->create();
-
-
+        UserCompanies::factory(10)->create();
     }
 }
